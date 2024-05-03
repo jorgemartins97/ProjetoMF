9466aeaf9eb27e830e2b44d9d433af3eccd72017
mmartins <marcelo.martins@b-simple.pt>
2024-05-03 17:50:29 +0100

    commit

---
+++ projects/projeto/projeto-main/projeto-main.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from '../src/app/home/home.component';
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  }
+];
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes)
+  ]
+})
+export class ProjetoMainModule { }
